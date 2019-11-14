cfc597e4c03dec1e8905dc774a1aa5eacd248d48
Markus Kowalewski <markus.kowalewski@fysik.su.se>
2019-11-14 19:38:29 +0100

    clean up release.nix

--- release.nix
+++ release.nix
@@ -7,46 +7,47 @@ let
   name = "NixOS-QChem";
   version = pin.version;
 
-in with buildPkgs; rec {
-  channel = stdenvNoCC.mkDerivation rec {
-    inherit name version;
-
-    src = ./.;
-
-    phases = [ "installPhase" ];
-
-    nixpkgsSource = fetchFromGitHub {
-      owner = "NixOS";
-      repo = "nixpkgs-channels";
-      inherit (pin.nixpkgs) rev sha256;
-    };
-
-    overlaySource = fetchFromGitHub {
-      owner = "markuskowa";
-      repo = "NixOS-QChem";
-      inherit (pin.NixOS-QChem) rev sha256;
-    };
-
-    installPhase = ''
-      mkdir -p $out/nixpkgs $out/NixOS-QChem
-
-      cp -r ${nixpkgsSource.outPath}/* $out/nixpkgs
-      cp -r ${overlaySource.outPath}/* $out/NixOS-QChem
-
-      cp ${src}/version.json $out/
-
-      cp ${src}/channel.nix $out/default.nix
-
-      echo "${version}" > $out/nixpkgs/.version
-    '';
+  nixpkgsSource = buildPkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs-channels";
+    inherit (pin.nixpkgs) rev sha256;
   };
 
+  overlaySource = buildPkgs.fetchFromGitHub {
+    owner = "markuskowa";
+    repo = "NixOS-QChem";
+    inherit (pin.NixOS-QChem) rev sha256;
+  };
+
+in with buildPkgs; rec {
+
+  # Create the channel structure
+  channel = runCommand "channel" {
+    inherit version;
+    versionsJSON = ./version.json;
+    channelNix = ./channel.nix;
+  }
+  ''
+    mkdir -p $out/nixpkgs $out/NixOS-QChem
+
+    cp -r ${nixpkgsSource.outPath}/* $out/nixpkgs
+    cp -r ${overlaySource.outPath}/* $out/NixOS-QChem
+
+    cp $versionsJSON $out/versions.json
+
+    cp $channelNix $out/default.nix
+
+    echo "${version}" > $out/nixpkgs/.version
+  '';
+
+
+  # Create nixexprs package for publishing the channel
   nixexprs = runCommand "nixeprs" {
     inherit version;
   } ''
 
       revNix=`echo ${pin.nixpkgs.rev} | cut -c1-6`
-      revQC=`echo ${pin.nixOS-QChem.rev} | cut -c1-6`
+      revQC=`echo ${pin.NixOS-QChem.rev} | cut -c1-6`
       release="${name}-${version}-$revNix-$revQC"
       mkdir -p $release $out
 
